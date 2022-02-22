798815e91365430ed132be3b178e2e710bfec1d4
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-22 18:43:26 -0500

    Use classNames library to conditionally compose a list of classes for list item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,10 +4,18 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  const {name, spots, setDay} = props;
+  const {name, spots, setDay, selected} = props;
+
+  let dayClass = classNames(
+    "day-list__item",
+    (!spots ? "day-list__item--full" : ""),
+    {"day-list__item--selected": selected} 
+  )
 
   return (
-    <li onClick={() => setDay(name)}>
+    <li 
+    onClick={() => setDay(name)}
+    className={dayClass}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
