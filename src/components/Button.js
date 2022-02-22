23ecd1694547a67987a6cd5300b63ea3dc7217d3
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-22 16:41:51 -0500

    Use classNames library to conditionally compose a list of classes for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,18 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
    
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
-
+   let buttonClass = classNames(
+      "button",
+      { 
+         "button--confirm": props.confirm,
+         "button--danger": props.danger,
+      }
+   )
+   
    return (
       <button onClick={props.onClick} disabled={props.disabled} className={buttonClass}>{props.children}</button>
    );
