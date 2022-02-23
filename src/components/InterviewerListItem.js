895c46dcd356ab103d326eb6324e427b3bf431a9
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-23 15:36:14 -0500

    Replace hardcoded values with props passed, add event listener on li, conditionally add classes and render interviewr's name if interviewer is selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,27 @@
 import React from "react";
 
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
+
+  const {id, name, avatar, selected, setInterviewer} = props;
+
+  let interviewerClass = classNames(
+    "interviewers__item",
+    {"interviewers__item--selected": selected}
+  )
   
   return (
-    <li className="interviewers__item">
+    <li 
+      onClick={() => {setInterviewer(id)}}
+      className={interviewerClass}>
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
+      src={avatar}
+      alt={name}
     />
-    Sylvia Palmer
+    {selected && name}
     </li>
   );
 }
