023e1b59f0f6cbf10ac766e855bc79b6693bc6b7
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-22 19:58:20 -0500

    Map over passed with props days array to produce an array of DaysListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-import "components/DayListItem";
+import DayListItem from "./DayListItem";
 
 
 export default function DayList(props) {
-  
-  return (
-   <ul></ul>
-  );
+  const {days, day, setDay} = props;
+
+  const daysList = days.map(dayInList => (
+    <DayListItem 
+    key={dayInList.id}
+    name={dayInList.name}
+    spots={dayInList.spots} 
+    selected={dayInList.name === day}
+    setDay={setDay}/>
+  ));
+
+ return (
+   <ul>
+     {daysList}
+   </ul>
+   );
 }
