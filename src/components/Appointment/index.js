083c381ad798b5690e5a276eff7342b6ba5e1b64
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-24 14:28:36 -0500

    Add Header component rendering into Appointment with time passed as props, and condittionally render Show and Empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,17 @@ import React from "react";
 
 import "./styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
 
-  const appointmentMsg = (props.time ? `Appointment at ${props.time}` : "No Appointments")
+  // const appointmentMsg = (props.time ? `Appointment at ${props.time}` : "No Appointments")
   return (
-    <article className="appointment">{appointmentMsg}</article>
+    <article className="appointment">{/*appointmentMsg*/}
+    <Header time={props.time}/>
+    {(props.interview ? <Show/> : <Empty/>)}
+    </article>
   );
 }
