431543951dd1e8f6c0b10bcd45d5d053e0995783
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-23 21:22:53 -0500

    Add a story for Status component chained to Appointment component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
 
 // Button Stories
 storiesOf("Button", module)
@@ -175,3 +176,4 @@ storiesOf("InterviewerList", module)
       onCancel={action("onCancel")}
     />
   ))
+  .add("Status", () => <Status message="Deleting"/>)
