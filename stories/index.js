bba011f63700f1cb30884b2a75cd247070938880
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-23 20:01:14 -0500

    Define stories for Appointment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 // Button Stories
 storiesOf("Button", module)
@@ -141,4 +142,15 @@ storiesOf("InterviewerList", module)
       interviewers={interviewers}
       onChange={action("setInterviewer")}
     />
+  ));
+
+  // Appointment stories
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => (
+  <Appointment time="12pm"/>
   ));
