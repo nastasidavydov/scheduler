9e3c6d42085d9902d63735dd759fbf78026ce6cb
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-24 15:47:50 -0500

    Add mock data for appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,7 +23,46 @@ const days = [
   },
 ];
 
-export default function Application(props) {
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+export default function Application() {
 
   const [day, setDay] = useState("Monday");
 
