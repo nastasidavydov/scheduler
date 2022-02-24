bf244c223368442a0d22948d157a4a8760af050f
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-24 01:05:03 -0500

    Use a controlled component in <input> to update student state when onChange event is triggered

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,34 +5,34 @@ import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
 
+  const {student, interviewer, interviewers, onSave, onCancel} = props;
+
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-return (
-<main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
+  return (
+  <main className="appointment__card appointment__card--create">
+    <section className="appointment__card-left">
+      <form autoComplete="off">
+        <input
+          className="appointment__create-input text--semi-bold"
+          name="name"
+          type="text"
+          placeholder="Enter Student Name"
+          value={student}
+          onChange={(event) => setStudent(event.target.value)}
+        />
+      </form>
+      <InterviewerList 
+        /* your code goes here */
       />
-    </form>
-    <InterviewerList 
-      /* your code goes here */
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger {/* your code goes here */}>Cancel</Button>
-      <Button confirm {/* your code goes here */}>Save</Button>
     </section>
-  </section>
-</main>
+    <section className="appointment__card-right">
+      <section className="appointment__actions">
+        <Button danger {/* your code goes here */}>Cancel</Button>
+        <Button confirm {/* your code goes here */}>Save</Button>
+      </section>
+    </section>
+  </main>
   );
 }
