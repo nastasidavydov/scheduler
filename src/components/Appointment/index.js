ba1a14034e1a9f92492d61fd22864df3816363b4
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-23 20:02:23 -0500

    Render conditionally appointment messaged based on props.time passed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import "Appointment/styles.scss"
+import "./styles.scss";
 
 export default function Appointment(props) {
 
+  const appointmentMsg = (props.time ? `Appointment at ${props.time}` : "No Appointments")
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{appointmentMsg}</article>
   );
 }
