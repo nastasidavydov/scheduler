671c8a9246c115fd86966c1ed64e9f297eb22ba4
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-24 00:56:27 -0500

    Add a default values for student and inerviewer states

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
 return (
 <main className="appointment__card appointment__card--create">
