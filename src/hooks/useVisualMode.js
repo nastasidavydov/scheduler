4b7ea5df6ccae1d2eb288bff7c3d789af64c5de9
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-26 14:25:38 -0500

    Add a transition fn within useVisualMode custom hook to update current mode to a new mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,8 +4,15 @@ export default function useVisualMode(initialMode) {
 
 const [mode, setMode] = useState(initialMode);
 
+//update current mode to a new mode
+const transition = newMode => {
+  const mode = setMode(newMode);
+  return mode
+}
+
 return {
-  mode
+  mode,
+  transition,
 }
 
 }
