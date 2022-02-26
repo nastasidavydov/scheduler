a93c7235072f36151a3012656977532057b1bb5c
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-26 15:54:15 -0500

    Add line to transition fn that add new mode to the hiftory state arr,and update back fn to remove the last el of history and set mode to be the last el of history arr

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 
-export default function useVisualMode(initialMode) {
+export default function useVisualMode(initial) {
 
-const [mode, setMode] = useState(initialMode);
+const [mode, setMode] = useState(initial);
 const [history, setHistory] = useState([initial]); 
 
 //update current mode to a new mode
 const transition = newMode => {
   setMode(newMode);
-  return mode
+  //add new mode to the history state array
+  setHistory((prev) => [...prev, newMode])
+  
 }
-
+// treansitions back to prev mode
 const back = () => {
-
+  //remove the last el of array and set mode to be the last el of history
+  history.pop()
+  setMode(history[history.length-1])
 }
 
 
