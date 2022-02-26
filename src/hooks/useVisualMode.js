07ac4f95ec163f9a5ac59cb6c49aa8fd324b5c6e
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-26 15:17:29 -0500

    Add a useState hook to keep track of the history of modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
 
 const [mode, setMode] = useState(initialMode);
+const [history, setHistory] = useState([initial]); 
 
 //update current mode to a new mode
 const transition = newMode => {
