8a5a17479ecd6abea9a227bae8671cf88f15e741
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-26 17:04:04 -0500

    Update setHistory in transition fn to skip adding new mode to history if replace is true

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,19 +6,26 @@ const [mode, setMode] = useState(initial);
 const [history, setHistory] = useState([initial]); 
 
 //update current mode to a new mode
-const transition = newMode => {
+const transition = (newMode, replace = false) => {
   setMode(newMode);
   //add new mode to the history state array
-  setHistory((prev) => [...prev, newMode])
-  
+  setHistory((prev) => {
+    
+    // if replace is true skip adding new mode to history
+    if (replace) {
+      
+      return [...prev];
+    }
+    return [...prev, newMode];
+  })
   
 }
 // treansitions back to prev mode
 const back = () => {
-  
+
   //prevents to go back past the initial mode
   if (history.length > 1) {
-    console.log(mode)
+    
   //remove the last el of array and set mode to be the last el of history
     history.pop()
     setMode(history[history.length-1])
