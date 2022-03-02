3653155f7443998ffba63e7ed1a7f48e1aa4b6e5
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-02 15:20:27 -0500

    Add a test case that checks submission the name entered by the user and replace calls onSave function when the name is defined with can successfully save after trying to submit an empty student name

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -47,24 +47,42 @@ describe("Form", () => {
   });
   
 
-  it("calls onSave function when the name is defined", () => {
-
-    /* 1. Create the mock onSave function */
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    const { queryByText, getByText } = render(<Form interviewers={interviewers} studentName="Lydia Miller-Jones" onSave={onSave}/>);
-
-    /* 3. Click the save button */
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+  
     fireEvent.click(getByText("Save"));
-
-    /* 1. validation is not shown */
+  
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Save"));
+  
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
-    /* 2. onSave is called once*/
     expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
   
-    /* 3. onSave is called with the correct arguments */
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+  
+    expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 });
