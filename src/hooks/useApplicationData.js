4f0d60d0757db731525ccb58dc244c4fadbafec2
Nastasi Davydov <nastasidavydov@gmail.com>
2022-03-05 18:19:16 -0500

    Add comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-
+  // set initial state as unified state object
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -12,26 +12,28 @@ export default function useApplicationData() {
 
   const setDay = day => setState({ ...state, day });
 
+
   useEffect(() => {
+    // fetches data from DB via API server
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ])
     .then((all) => {
-      
+      // update app state with the data received
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       
     })
   }, [])
 
-  //updates days arr on partucular day spot change on delete/add interview
+  //updates days arr on particular day spot change on delete/add interview
   const updateSpots = (id, action) => {
-
+    // handles add or subtract logic 
     let spot = 0;
     if (action === "add") spot--;
     else spot++;
-
+    // creates new days array with updated spots for appropriate day
     const days = [...state.days].map(day => {
 
       if (day.appointments.includes(id)) {
@@ -43,9 +45,9 @@ export default function useApplicationData() {
     
   }
 
-// add interview appointment 
+  // add interview appointment 
   function bookInterview(id, interview) {
-
+    // new or updated state objects
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -55,18 +57,17 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    
+
     return axios.put(`/api/appointments/${id} `, appointment)
       .then(() => {
-        console.log(appointments)
+        // on creates only (not edits), call update spots to subtract one spot and store new days array
         const days = (state.appointments[id].interview === null) ? updateSpots(id, 'add') : state.days
-        console.log(days)
-        setState({
-          ...state,
+        
+        setState(prev => ({
+          ...prev,
           appointments,
           days
-          
-        })
+        }))
         }
       )
   }
@@ -86,8 +87,9 @@ export default function useApplicationData() {
     
     return axios.delete(`/api/appointments/${id} `, appointment)
       .then((res) => {
+        // call update spots to add one spot and store new days array
         const days = (state.appointments[id].interview !== null) ? updateSpots(id, 'delete') : state.days
-        console.log(appointments[id])
+        
         setState({
           ...state,
           appointments,
